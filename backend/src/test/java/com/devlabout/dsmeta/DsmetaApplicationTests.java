bd2e6aa59dcba0ac891e6f25d2f53a4af3a0e43e
Jeferson-Labout <jefson1989@gmail.com>
2022-07-15 05:39:01 -0300

    Project created

---
+++ backend/src/test/java/com/devlabout/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlabout.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
