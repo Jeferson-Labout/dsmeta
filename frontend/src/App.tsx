fc5a7a2decbea0539c94ee405caa5987ac245eb8
Jeferson-Labout <jefson1989@gmail.com>
2022-07-15 06:46:10 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
-import NotificationButton from "./components/notificationButton"
+import Header from "./components/Header"
+import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo</h1>
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+
+
+          </div>
+        </section>
+      </main>
     </>
   )
 }
