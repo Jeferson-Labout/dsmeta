3976efa00a01d51afb8f76ad5dbb1464fc1c2e8e
Jeferson-Labout <jefson1989@gmail.com>
2022-07-16 07:24:12 -0300

    API test

---
+++ backend/src/main/java/com/devlabout/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,10 @@
+package com.devlabout.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devlabout.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long>{
+	
+
+}
---
+++ backend/src/main/java/com/devlabout/dsmeta/services/SaleService.java
@@ -0,0 +1,22 @@
+package com.devlabout.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devlabout.dsmeta.entities.Sale;
+import com.devlabout.dsmeta.repositories.SaleRepository;
+@Service
+public class SaleService {
+
+	
+	@Autowired
+	private SaleRepository repository;
+	
+	public List<Sale> findSales() {
+		return repository.findAll();	
+		
+	}
+	
+}
